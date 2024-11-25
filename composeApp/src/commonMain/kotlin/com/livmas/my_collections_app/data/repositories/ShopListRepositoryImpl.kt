9cff8e53ef1333fcdaf640db3304ebe694a0ccca
livmas <seiranyan.sg@gmail.com>
2024-11-25 11:47:48 +0300

    fetchAuthorized used in all methods of ShopListRepositoryImpl

--- composeApp/src/commonMain/kotlin/com/livmas/my_collections_app/data/repositories/ShopListRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/com/livmas/my_collections_app/data/repositories/ShopListRepositoryImpl.kt
@@ -13,53 +13,32 @@ import com.livmas.my_collections_app.domain.models.ShopListInfo
 import com.livmas.my_collections_app.domain.models.ShoppingItem
 import com.livmas.my_collections_app.domain.repositories.ShopListRepository
 import com.livmas.my_collections_app.mappers.toDomain
-import com.livmas.my_collections_app.utils.Resource
 import com.livmas.my_collections_app.utils.ResourceFlow
-import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.flow
 
 class ShopListRepositoryImpl(
     private val listRemoteDataSource: ListRemoteDataSource,
     private val userRemoteDataSource: UserRemoteDataSource,
 ): ShopListRepository, BaseAuthorizedRepository(userRemoteDataSource) {
     override suspend fun getShopLists(): ResourceFlow<List<ShopListInfo>> {
-        return flow {
-            emit(Resource.Loading)
-            try {
-                val key = userRemoteDataSource.getUserKey()
-                val result = listRemoteDataSource.getShopLists(key).shopList.map {
-                    it.toDomain()
-                }
+        return fetchAuthorized { authKey ->
+            val response = listRemoteDataSource.getShopLists(authKey)
 
-                emit(Resource.Success(result))
-            }
-            catch (t: Throwable) {
-                println(t.stackTraceToString())
-                emit(Resource.Error(t))
-            }
+            check (response.success)
+            response.shopList.map { it.toDomain() }
         }
     }
 
     override suspend fun createShopList(shopListInfo: ShopListInfo): ResourceFlow<ShopListInfo> {
-        return flow {
-            emit(Resource.Loading)
-            try {
-                val key = userRemoteDataSource.getUserKey()
-                val result = listRemoteDataSource.createShopList(
-                    authKey = key,
-                    request = CreateListRequest(shopListInfo.name)
-                )
+        return fetchAuthorized { authKey ->
+            val result = listRemoteDataSource.createShopList(
+                authKey = authKey,
+                request = CreateListRequest(shopListInfo.name)
+            )
 
-                if (!result.success)
-                    throw Throwable(message = "Not success: $result")
-
-                emit(Resource.Success(shopListInfo.copy(
-                    id = result.listId
-                )))
-            }
-            catch (t: Throwable) {
-                emit(Resource.Error(t))
-            }
+            check (result.success)
+            shopListInfo.copy(
+                id = result.listId
+            )
         }
     }
 
@@ -70,50 +49,30 @@ class ShopListRepositoryImpl(
                 request = DeleteListRequest(shopListInfo.id)
             )
 
-            if (!result.newValue)
-                throw IllegalStateException("Not success")
+            check (result.newValue)
             result
         }
     }
 
     override suspend fun getListContent(listId: Long): ResourceFlow<List<ShoppingItem>> {
-        return flow {
-            emit(Resource.Loading)
+        return fetchAuthorized { authKey ->
+            val result = listRemoteDataSource.getListContent(authKey, GetListContentRequest(listId))
 
-            try {
-                val key = userRemoteDataSource.getUserKey()
-                val result = listRemoteDataSource.getListContent(key, GetListContentRequest(listId))
-
-                if (!result.success)
-                    throw Throwable(message = "Not success: $result")
-
-                emit(Resource.Success(result.items.map { it.toDomain() }))
-            }
-            catch (t: Throwable) {
-                emit(Resource.Error(t))
-            }
+            check (result.success)
+            result.items.map { it.toDomain() }
         }
     }
 
     override suspend fun deleteListItem(listId: Long, itemId: Long): ResourceFlow<Unit> {
-        return flow {
-            emit(Resource.Loading)
-            try {
-                val result = listRemoteDataSource.deleteListItem(
-                    authKey = userRemoteDataSource.getUserKey(),
-                    request = DeleteListItemRequest(
-                        listId, itemId
-                    )
+        return fetchAuthorized {
+            val result = listRemoteDataSource.deleteListItem(
+                authKey = userRemoteDataSource.getUserKey(),
+                request = DeleteListItemRequest(
+                    listId, itemId
                 )
+            )
 
-                if (!result.success)
-                    throw Throwable(message = "Not success: $result")
-
-                emit(Resource.Success(Unit))
-            }
-            catch (t: Throwable) {
-                emit(Resource.Error(t))
-            }
+            check (result.success)
         }
     }
 
