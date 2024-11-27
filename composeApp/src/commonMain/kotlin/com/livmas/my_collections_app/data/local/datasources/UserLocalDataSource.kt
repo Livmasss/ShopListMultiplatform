d20731b62c15030ba7508a5073bd2a8ae10add1e
livmas <seiranyan.sg@gmail.com>
2024-11-27 17:47:07 +0300

    Task Finished

--- composeApp/src/commonMain/kotlin/com/livmas/my_collections_app/data/local/datasources/UserLocalDataSource.kt
+++ composeApp/src/commonMain/kotlin/com/livmas/my_collections_app/data/local/datasources/UserLocalDataSource.kt
@@ -11,17 +11,18 @@ import kotlinx.coroutines.flow.first
 class UserLocalDataSource(
     private val dataStore: DataStore<Preferences> = createDataStore()
 ) {
+//    It should be encrypted to be more secure
     suspend fun saveUserKey(authKey: String) {
         Logger.d { "Auth key saving: $authKey" }
         dataStore.edit {
             it[USER_KEY] = authKey
         }
     }
+
     suspend fun restoreUserKey(): String {
         val pref = dataStore.data.first()
         val authKey = pref[USER_KEY]
         Logger.d { "Auth key restored: $authKey" }
         return authKey ?: throw IllegalStateException("$USER_KEY cant be restored from UserLocalDataSource")
-
     }
 }
